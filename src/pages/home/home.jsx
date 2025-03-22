cbc545a5f3ec9bddc25d43e9734911bacde92a23
Mr.Vasanth <145196660+Mrvasanth93@users.noreply.github.com>
2025-03-22 20:20:30 +0530

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,24 @@
 import "./home.css"
 import profile from "../../assets/0Z6A1905.jpg"
+import { useEffect, useState } from 'react';
 const Home = () =>{
+    const roles = ["Web Designer", "Full Stack Developer", "Frontend Developer", "Backend Developer"];
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % roles.length);
+    }, 3000);
+
+    return () => clearInterval(interval); // cleanup
+  }, []);
     return(
         <>
             <div id="home" className="home">
                 <div className="left-or-top">
                     <h1 className="h11">Hi,</h1>
                     <h1 className="h12">I am <span>Vasanth</span></h1>
-                    <h1 className="h13">Web Designer</h1>
+                    <h1 className="h13">roles[index]</h1>
                 </div>
                 <div className="right-or-bottom">
                     <div className="profile-img">
