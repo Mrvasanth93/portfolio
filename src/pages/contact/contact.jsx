db3df250101de883569b04334d6a8eef5178991d
Mr.Vasanth <145196660+Mrvasanth93@users.noreply.github.com>
2025-03-22 20:29:04 +0530

    Update contact.jsx

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,5 +1,9 @@
 import "./contact.css"
+import { useState } from 'react';
+
 const Contact = () => {
+    const [subject,setSubject] = useState(");
+    const [message,SetMessage] = usestate ("");                                       
     return (
         <>
             <div id="contact" className="contact-tittle">
@@ -9,11 +13,13 @@ const Contact = () => {
                 <div className="contact-form">
                     <div><h6>Name</h6></div>
                     <div className="name"><input placeholder="Vasanth" type="text" /></div>
-                    <div><h6>Name</h6></div>
+                    <div><h6>Email</h6></div>
                     <div className="email"><input type="email" placeholder="abc@gmail.com"/></div>
-                    <div><h6>Name</h6></div>
+                    <div><h6>Subject</h6></div>
+                    <div className="email"><input onChange={(e)=>{setSubject(e.taget.value)}</input>} type="text" placeholder="Subject for your Email"/></div>
+                    <div><h6>Message</h6></div>
                     <div className="message"><textarea type="text" placeholder="write something"/></div>
-                    <div className="btn"><h6><a target="_blank" href="mailto:mrvasanth93@gmail.com?subject=hellovasanth&body=hellovasanthpleasecontacstme">Contact</a></h6></div>
+                    <div className="btn"><h6><a target="_blank" href='mailto:mrvasanth93@gmail.com?subject=${subject}&body=hellovasanthpleasecontacstme'>Contact</a></h6></div>
                 </div>
             </div>
         </>
