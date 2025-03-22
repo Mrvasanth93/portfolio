3ab1886f14c373e55a3494fbee58a3a5834e4556
Mrvasanth93 <mrvasanth93@gmail.com>
2025-03-22 17:12:29 +0530

    after animation

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -8,9 +8,9 @@ const Home = () =>{
         <>
             <div id="home" className="home">
                 <div className="left-or-top">
-                    <h1>Hi,</h1>
-                    <h1>I am <span>Vasanth</span></h1>
-                    <h1>Web Designer</h1>
+                    <h1 className="h11">Hi,</h1>
+                    <h1 className="h12">I am <span>Vasanth</span></h1>
+                    <h1 className="h13">Web Designer</h1>
                 </div>
                 <div className="right-or-bottom">
                     <div className="profile-img">
