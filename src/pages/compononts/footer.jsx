467a1181e54add57d07fd62842f78db1bfc82c46
Mrvasanth93 <mrvasanth93@gmail.com>
2025-03-23 07:53:39 +0530

    small update

--- src/pages/compononts/footer.jsx
+++ src/pages/compononts/footer.jsx
@@ -10,7 +10,7 @@ const Footer = () =>{
             <div className="footer-section">
                 <h1>Vasanth Kumar</h1>
                 <div className="social-medias">
-                    <div className="media"><a target="_blank" href="https://wa.me/9345488130?text=hello vasanth"><img  src={whatsapp} alt="" /></a></div>
+                    <div className="media"><a target="_blank" href="https://wa.me/9345488130?text=hello vasanth "><img  src={whatsapp} alt="" /></a></div>
                     <div className="media"><a target="_blank" href="https://www.instagram.com/vasanth_7_8/"><img src={instagram} alt="" /></a></div>
                     <div className="media"><a target="_blank" href="https://Mrvasanth93.github.io/portfolio"><img src={website} alt="" /></a></div>
                     <div className="media"><a target="_blank" href="https://github.com/Mrvasanth93"><img src={github} alt="" /></a></div>
--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
 import "./contact.css"
-import { useState } from 'react';
 
 const Contact = () => {
+    const [name,setName] = useState("");
+    const [mail,setMail] = useState("");
+    const [subject,setSubject] = useState("");
+    const [message,SetMessage] = useState ("");
     return (
         <>
             <div id="contact" className="contact-tittle">
@@ -10,13 +13,17 @@ const Contact = () => {
             </div>
             <div className="contact">
                 <div className="contact-form">
+                    <form action="" >
                     <div><h6>Name</h6></div>
-                    <div className="name"><input placeholder="Vasanth" type="text" /></div>
-                    <div><h6>Name</h6></div>
-                    <div className="email"><input type="email" placeholder="abc@gmail.com"/></div>
-                    <div><h6>Name</h6></div>
-                    <div className="message"><textarea type="text" placeholder="write something"/></div>
-                    <div className="btn"><h6><a target="_blank" href="mailto:mrvasanth93@gmail.com?subject=hellovasanth&body=hellovasanthpleasecontacstme">Contact</a></h6></div>
+                    <div className="name"><input onChange={(e)=>{setName(e.target.value)}} placeholder="Vasanth" type="text" /></div>
+                    <div><h6>Email</h6></div>
+                    <div className="email"><input onChange={(e)=>{setMail(e.target.value)}} type="email" placeholder="abc@gmail.com"/></div>
+                    <div><h6>Subject</h6></div>
+                    <div className="email"><input onChange={(e)=>{setSubject(e.target.value)}} type="email" placeholder="subject"/></div>
+                    <div><h6>Message</h6></div>
+                    <div className="message"><textarea onChange={(e)=>{SetMessage(e.target.value)}} type="text" placeholder="write something"/></div>
+                    <div className="btn"><h6><a target="_blank" href= {`mailto:mrvasanth93@gmail.com?subject=${subject}/${name}&body=${message}`} >Contact</a></h6></div>
+                    </form>
                 </div>
             </div>
         </>
